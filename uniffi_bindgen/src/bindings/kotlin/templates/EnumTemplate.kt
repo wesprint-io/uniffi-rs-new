5c1dc00df350ff3cd2ffd65fd6e5c66d719188cb
Cyril Mottier <cyril@amo.co>
2024-07-02 15:53:48 +0200

    android: get rid of the added Companion objects

--- uniffi_bindgen/src/bindings/kotlin/templates/EnumTemplate.kt
+++ uniffi_bindgen/src/bindings/kotlin/templates/EnumTemplate.kt
@@ -13,7 +13,6 @@ enum class {{ type_name }} {
     {%- call kt::docstring(variant, 4) %}
     {{ variant|variant_name }}{% if loop.last %};{% else %},{% endif %}
     {%- endfor %}
-    companion object
 }
 
 public object {{ e|ffi_converter_name }}: FfiConverterRustBuffer<{{ type_name }}> {
@@ -44,9 +43,7 @@ sealed class {{ type_name }}{% if contains_object_references %}: Disposable {% e
         {%- call kt::docstring(field, 8) %}
         val {{ field.name()|var_name }}: {{ field|type_name(ci) }}{% if loop.last %}{% else %}, {% endif %}
         {% endfor -%}
-    ) : {{ type_name }}() {
-        companion object
-    }
+    ) : {{ type_name }}()
     {%- endif %}
     {% endfor %}
 
@@ -66,7 +63,6 @@ sealed class {{ type_name }}{% if contains_object_references %}: Disposable {% e
         }.let { /* this makes the `when` an expression, which ensures it is exhaustive */ }
     }
     {% endif %}
-    companion object
 }
 
 public object {{ e|ffi_converter_name }} : FfiConverterRustBuffer<{{ type_name }}>{
